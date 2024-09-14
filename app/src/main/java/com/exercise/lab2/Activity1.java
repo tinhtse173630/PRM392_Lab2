dd921be2885c8e697095ef758e83baaa6f59b762
tinbusiness.work <tinbusiness.work@gmail.com>
2024-09-14 19:34:19 +0700

    clear trash import

--- app/src/main/java/com/exercise/lab2/Activity1.java
+++ app/src/main/java/com/exercise/lab2/Activity1.java
@@ -8,9 +8,6 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
 
